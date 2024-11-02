00633d22fc37d1c7796764aee82b07c78457d027
Sithun76 <prabashasithun962@gmail.com>
2024-11-02 23:42:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CmgkiBCL#OnIR98A_6bEjHOwAzvQB2NrefqQlY0UiMOEJEcpatjs",
+module.exports = {𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rv52ASzY#8zDxZw3t5xvn9rPL30_43ldKiqU-bGHrvbEqX3lQwqI
+SESSION_ID: process.env.SESSION_ID || "",
 MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
